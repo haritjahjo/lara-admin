ab029f9e19bb7ee116b301ae53241ddcccb634f8
Hari Tjahjo <haritjahjo@gmail.com>
2023-02-05 08:01:04 +0700

    Add user and sprecify user roles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,13 +5,54 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-6 m-6">
+
+            <div class="w-full m-4">
+                @role('writer|admin')
+                    <a href="#" class="m-2 p-2 bg-green-400 rounded">
+                        Create New Post
+                    </a>
+                @endrole
+            </div>
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50 dark:bg-gray-600 dark:text-gray-200">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Id</th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Title</th>
+                        <th scope="col" class="relative px-6 py-3">Edit</th>
+                    </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                    <tr></tr>
+                    @foreach (App\Models\Post::all() as $post)
+                        <tr>
+                        <td class="px-6 py-4 whitespace-nowrap">{{ $post->id}}</td>
+                        <td class="px-6 py-4 whitespace-nowrap">{{ $post->title}}</td>
+                        <td class="px-6 py-4 text-right text-sm">
+                            <a href="#" class="m-2 p-2 bg-green-400 rounded ">Edit</a>
+                            <a href="#" class="m-2 p-2 bg-green-600 rounded ">Publish</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                    
+                    <!-- More items... -->
+                    </tbody>
+                </table>
+                <div class="m-2 p-2">Pagination</div>
+        </div>
+  </div>
+</div>
+
+        {{-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 </x-app-layout>
